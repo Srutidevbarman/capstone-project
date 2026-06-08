38d978b5a40058c699ace28b5c9642dcd535ba79
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-06-08 19:27:41 +0530

    added template server

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
-
+import { v7 as uuid } from "uuid";
+import { createPod } from "./kubernates/pod.js";
+import { createService } from "./kubernates/service.js";
 const app = express();
 
 app.use(morgan("dev"));
@@ -11,5 +13,14 @@ app.get("/api/sandbox/health", (req, res) => {
   res.status(200).json({ message: "Sandbox API is healthy!", status: "ok" });
 });
 
+app.post("/api/sandbox/start", async (req, res) => {
+  const sandboxId = uuid();
+  await Promise.all([createPod(sandboxId), createService(sandboxId)]);
+  return res.status(200).json({
+    message: "Sandbox started successfully!",
+    sandboxId,
+    previewUrl: `http://${sandboxId}.preview.localhost`,
+  });
+});
 
 export default app;
---
+++ sandbox/server/src/kubernates/service.js
@@ -0,0 +1,32 @@
+import { k8sApi } from "./config.js";
+
+export async function createService(sandboxId) {
+  const serviceManifest = {
+    metadata: {
+      name: `sandbox-service-${sandboxId}`,
+      labels: {
+        app: "sandbox",
+        sandboxId: sandboxId,
+      },
+    },
+    spec: {
+      selector: {
+        app: "sandbox",
+        sandboxId: sandboxId,
+      },
+      ports: [
+        {
+          port: 5173,
+          targetPort: 5173,
+          name: "http",
+          protocol: "TCP",
+        },
+      ],
+    },
+  };
+  const response = await k8sApi.createNamespacedService(
+    "default",
+    serviceManifest,
+  );
+  return response;
+}
