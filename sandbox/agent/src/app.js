1ce7b26fd6689aa4c7e3db9ffe3e45d8ce396bdc
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-06-12 21:05:32 +0530

    added read file api

--- sandbox/agent/src/app.js
+++ sandbox/agent/src/app.js
@@ -20,4 +20,40 @@ app.get("/list-files", async (req, res) => {
   });
 });
 
+// @route GET /read-file
+// @desc Read the content of all the files requested in the query parameter "files" and return them as json object
+// -e.g. /read-file?files=file1.txt,/src/file2.txt
+
+app.get("/read-file", async (req, res) => {
+  const files = req.query.files;
+
+  if (!files) {
+    return res
+      .status(400)
+      .json({ message: "No files specified", status: "error" });
+  }
+
+  const fileList = files.split(",");
+
+  await Promise.all(
+    fileList.map(async (file) => {
+      const filePath = `${WORK_DIR}/${file}`;
+      try {
+        const content = await fs.promises.readFile(filePath, "utf-8");
+        return {
+          [filePath]: content,
+        };
+      } catch (err) {
+        return {
+          [filePath]: `Error reading file: ${err.message}`,
+        };
+      }
+    }),
+  );
+  res.status(200).json({
+    status: "success",
+    files: results,
+  });
+});
+
 export default app;
