5f5b7baa69f60acacae8166c581a9c4c1f0c2510
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-06-16 19:25:17 +0530

    intehrate skaffold for beter flow

--- ai-orchestration/src/agents/code.agent.js
+++ ai-orchestration/src/agents/code.agent.js
@@ -14,11 +14,4 @@ const agent = createAgent({
   tools: [listFiles, readFiles, updateFiles],
 });
 
-await agent.invoke({
-  messages: [
-    {
-      role: "user",
-      content: "change the theme to white",
-    },
-  ],
-});
+export default agent;
--- ai-orchestration/src/app.js
+++ ai-orchestration/src/app.js
@@ -8,4 +8,8 @@ app.get("/api/status/healthz", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
+app.get("/api/ai/healthz", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 export default app;
